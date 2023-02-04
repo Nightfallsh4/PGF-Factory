e40a1bfdac4a63861aa8450d45be782a63c2c8c5
Shanmugadevan <shanmugadevan@protonmail.com>
2023-02-04 14:35:59 +0530

    Added Biconomy Context and chainConfig

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css"
+import type { AppProps } from "next/app"
+import dynamic from "next/dynamic"
+const BiconomyContextProvider = dynamic(
+	() => import("../components/contexts/BiconomyContext"),
+	{
+		ssr: false,
+	},
+)
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+    // useContext(BiconomyContext) to get the account, socialLoginSDK, provider, smart account, connectWeb3 and disconnectWeb3 inside any component
+		<BiconomyContextProvider>
+			<Component {...pageProps} />
+		</BiconomyContextProvider>
+	)
 }
