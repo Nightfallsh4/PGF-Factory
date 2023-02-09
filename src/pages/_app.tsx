940bce80a378c544eab34d757dd4983b1efb7fe8
kenilshahh <kenilshah1505@gmail.com>
2023-02-09 22:10:57 +0530

    added create page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,7 @@ import "@/styles/globals.css"
 import { BrowserRouter } from "react-router-dom"
 // import type { AppProps } from "next/app"
 import dynamic from "next/dynamic"
-import CreateNFT from "./createnft"
-import Link from 'next/link'
+import CreateNFT from "./createNft"
 const BiconomyContextProvider = dynamic(
 	() => import("../components/contexts/BiconomyContext"),
 	{
@@ -15,7 +14,12 @@ export default function App({ Component, pageProps }) {
 	return (
     // useContext(BiconomyContext) to get the account, socialLoginSDK, provider, smart account, connectWeb3 and disconnectWeb3 inside any component
 		<BiconomyContextProvider>
-			
+			<div>
+      <BrowserRouter>
+	  
+        <CreateNFT />
+        </BrowserRouter>
+    </div>
 		</BiconomyContextProvider>
    
 	)
