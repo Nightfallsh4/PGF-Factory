87ba4b96a2e673dc2cdeaf38802e8881858125a5
Mohd Ziyad <mohdziyad@Mohds-MacBook-Air.local>
2023-02-11 01:32:18 +0530

    Events added

--- contracts/PGFFactory.sol
+++ contracts/PGFFactory.sol
@@ -6,88 +6,117 @@ import "@openzeppelin/contracts-upgradeable/access/AccessControlEnumerableUpgrad
 import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
-contract PGFFactory is AccessControlEnumerableUpgradeable, ReentrancyGuardUpgradeable, UUPSUpgradeable{
-
+contract PGFFactory is
+    AccessControlEnumerableUpgradeable,
+    ReentrancyGuardUpgradeable,
+    UUPSUpgradeable
+{
     // State Variable
     uint256 public s_creationFee;
     address public s_payoutAddress;
     uint256 public s_fundingId;
     mapping(uint => address) private s_fundingIdToAddress;
     uint256 public s_contractBalance;
-    
+
     // Constant variables
-    bytes32 public constant PAYOUTADDRESS_ROLE = keccak256("PAYOUTADDRESS_ROLE");
+    bytes32 public constant PAYOUTADDRESS_ROLE =
+        keccak256("PAYOUTADDRESS_ROLE");
     bytes32 public constant CREATIONFEE_ROLE = keccak256("CREATIONFEE_ROLE");
 
     // Events
-    event FundingCreated (
+    event FundingCreated(
         address indexed contractAddress,
         uint256 indexed totalFunding,
         uint256 indexed vestingPeriod,
         uint256 withdrawalFee
     );
 
-    event Withdrawed (
-        address indexed payoutAddress,
-        uint256 indexed amount
-    );
+    event Withdrawed(address indexed payoutAddress, uint256 indexed amount);
 
-    event CreationFeeChanged (
-        uint256 indexed newCreationFee
-    );
+    event CreationFeeChanged(uint256 indexed newCreationFee);
 
-    event PayoutAddressChanged (
-        address indexed newPayoutAddress
-    );
-
+    event PayoutAddressChanged(address indexed newPayoutAddress);
 
     // Modifiers
     modifier isEnoughEther() {
-        require(msg.value>= s_creationFee,"Ether sent should be equal or more than creation fees");
+        require(
+            msg.value >= s_creationFee,
+            "Ether sent should be equal or more than creation fees"
+        );
         _;
     }
 
-   function initialize(uint256 _creationFee, address _payoutAddress) initializer public {
+    function initialize(
+        uint256 _creationFee,
+        address _payoutAddress
+    ) public initializer {
         s_creationFee = _creationFee;
         s_payoutAddress = _payoutAddress;
         s_fundingId = 0;
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
 
-    function _authorizeUpgrade(address) internal override onlyRole(DEFAULT_ADMIN_ROLE) {}
+    function _authorizeUpgrade(
+        address
+    ) internal override onlyRole(DEFAULT_ADMIN_ROLE) {}
 
     // External Function
-    function createFunding(uint256 _totalFunding, uint256 _withdrawalFee, uint256 _vestingPeriod,bool _isGroupWithdrawal) external payable isEnoughEther {
-        FundingContract fundingContract = new FundingContract(_totalFunding,_withdrawalFee,_vestingPeriod,_isGroupWithdrawal);
+    function createFunding(
+        uint256 _totalFunding,
+        uint256 _withdrawalFee,
+        bool _isGroupWithdrawal,
+        string memory _tokenUri,
+        uint64 _startTimestamp,
+        uint64 _vestingPeriodInSeconds
+    ) external payable isEnoughEther {
+        FundingContract fundingContract = new FundingContract(
+            _totalFunding,
+            _withdrawalFee,
+            _isGroupWithdrawal,
+            _tokenUri,
+            _startTimestamp,
+            _vestingPeriodInSeconds
+        );
         s_fundingIdToAddress[s_fundingId] = address(fundingContract);
         s_fundingId++;
         s_contractBalance += msg.value;
-        emit FundingCreated(address(fundingContract), _totalFunding, _vestingPeriod, _withdrawalFee);
+        emit FundingCreated(
+            address(fundingContract),
+            _totalFunding,
+            _vestingPeriod,
+            _withdrawalFee
+        );
     }
 
     function withdraw() external nonReentrant {
         address payoutAddress = s_payoutAddress;
         uint256 contractBalance = s_contractBalance;
-        (bool success, ) = payable(payoutAddress).call{value:contractBalance}("");
-        require(success,"Withdraw not successfull");
+        (bool success, ) = payable(payoutAddress).call{value: contractBalance}(
+            ""
+        );
+        require(success, "Withdraw not successfull");
         s_contractBalance = 0;
         emit Withdrawed(payoutAddress, contractBalance);
     }
 
     // Setter Functions
-    function setCreationFee(uint256 _creationFee) external onlyRole(CREATIONFEE_ROLE) {
+    function setCreationFee(
+        uint256 _creationFee
+    ) external onlyRole(CREATIONFEE_ROLE) {
         s_creationFee = _creationFee;
         emit CreationFeeChanged(_creationFee);
     }
 
-    function setPayoutAddress(address _payoutAddress) external onlyRole(PAYOUTADDRESS_ROLE) {
-        require(_payoutAddress != address(0),"Address cant be zero");
+    function setPayoutAddress(
+        address _payoutAddress
+    ) external onlyRole(PAYOUTADDRESS_ROLE) {
+        require(_payoutAddress != address(0), "Address cant be zero");
         s_payoutAddress = _payoutAddress;
         emit PayoutAddressChanged(_payoutAddress);
     }
 
     // View Functions
-    function getAddress(uint256 _fundingId) external view returns(address){
+    function getAddress(uint256 _fundingId) external view returns (address) {
         return s_fundingIdToAddress[_fundingId];
     }
 }
--- deploy/00-deploy-PGFFactory.js
+++ deploy/00-deploy-PGFFactory.js
@@ -3,23 +3,23 @@ console.log("Starting deploy...")
 const { upgrades, ethers } = require("hardhat")
 // const ethers = require("ethers")
 module.exports = async ({ getNamedAccounts, deployments }) => {
-	const { deployer } = await getNamedAccounts()
-	const { deploy, log } = deployments
-	console.log("Staring argument")
-	const creationFee = ethers.utils.parseEther("0.0001")
-	const payoutAddress = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
+  const { deployer } = await getNamedAccounts()
+  const { deploy, log } = deployments
+  console.log("Staring argument")
+  const creationFee = ethers.utils.parseEther("0.0001")
+  const payoutAddress = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
 
-	const args = [creationFee, payoutAddress]
-	console.log("Getting contract")
-	const implementContract = await ethers.getContractFactory("PGFFactory")
-	log("-------------------------")
+  const args = [creationFee, payoutAddress]
+  console.log("Getting contract")
+  const implementContract = await ethers.getContractFactory("PGFFactory")
+  log("-------------------------")
 
-	const deployedProxy = await upgrades.deployProxy(implementContract, args, {
-		kind: "uups",
-	})
-	await deployedProxy.deployed()
-	log(deployedProxy.address)
-	log("-------------------------")
+  const deployedProxy = await upgrades.deployProxy(implementContract, args, {
+    kind: "uups",
+  })
+  await deployedProxy.deployed()
+  log(deployedProxy.address)
+  log("-------------------------")
 }
 
 module.exports.tags = ["PGFFactory", "all"]
