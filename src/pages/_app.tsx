533c2f4eb2e0b397302ce46ba815726e874b3f64
Mohd Ziyad <mohdziyad@Mohds-MacBook-Air.local>
2023-02-17 23:10:21 +0530

    Minor changes to frontend

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,34 +4,33 @@ import { BrowserRouter, Route, Routes } from "react-router-dom"
 import dynamic from "next/dynamic"
 import CreateNFT from "./createNft"
 // import CampaignDetails from "./CampaignDetails";
-import CreateCampaign from "./CreateCampaign";
-import Homee from "./Home";
-import { FundCard } from "./components";
-import  DisplayCampaigns from "./DisplayCampaigns";
+import CreateCampaign from "./CreateCampaign"
+import Homee from "./Home"
+import { FundCard } from "./components"
+import DisplayCampaigns from "./DisplayCampaigns"
 const BiconomyContextProvider = dynamic(
-	() => import("../contexts/BiconomyContext"),
-	{
-		ssr: false,
-	},
+    () => import("../contexts/BiconomyContext"),
+    {
+        ssr: false,
+    }
 )
 
 export default function App({ Component, pageProps }) {
-	return (
-    // useContext(BiconomyContext) to get the account, socialLoginSDK, provider, smart account, connectWeb3 and disconnectWeb3 inside any component
-		<BiconomyContextProvider>
-			<BrowserRouter>
-			<CreateNFT/>
-			<div>
-				<Routes>
-					{/* <CreateNFT/> */}
-          {/* <Route path="/" element={<Homee/>} /> */}
-          {/* <Route path="myNfts" element={<Profile />} /> */}
-          <Route path="/" element={<CreateCampaign />} />
-          {/* <Route path="/contract-details/:id" element={<CampaignDetails/>} /> */}
-        </Routes>
-				</div>
-				</BrowserRouter>
-		</BiconomyContextProvider>
-   
-	)
+    return (
+        // useContext(BiconomyContext) to get the account, socialLoginSDK, provider, smart account, connectWeb3 and disconnectWeb3 inside any component
+        <BiconomyContextProvider>
+            <BrowserRouter>
+                <CreateNFT />
+                <div>
+                    <Routes>
+                        {/* <CreateNFT /> */}
+                        {/* <Route path="/" element={<Homee />} /> */}
+                        {/* <Route path="myNfts" element={<Profile />} /> */}
+                        <Route path="/" element={<CreateCampaign />} />
+                        {/* <Route path="/contract-details/:id" element={<CampaignDetails/>} /> */}
+                    </Routes>
+                </div>
+            </BrowserRouter>
+        </BiconomyContextProvider>
+    )
 }
