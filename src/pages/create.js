a3f42dc600a8b8044985da702958b4543c561808
kenilshahh <kenilshah1505@gmail.com>
2023-02-19 19:41:20 +0530

    omage uplading

--- src/pages/create.js
+++ src/pages/create.js
@@ -95,7 +95,14 @@ function CreateCampaign() {
 
     const [arr, setArr] = useState(inputArr)
     const [clicked, setClicked] = useState(false)
+const [image, setImage] = useState(null)
 
+const handleImageChange = (event) => {
+    if (event.target.files && event.target.files[0]) {
+        const selectedImage = event.target.files[0]
+        setImage(URL.createObjectURL(selectedImage))
+    }
+}
 
     const addInput = () => {
         setArr((s) => {
@@ -121,20 +128,20 @@ function CreateCampaign() {
         })
     }
 
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        // checkIfImage(form.image, async (exists) => {
-        //     if (exists) {
-        //         setIsLoading(true)
-        //         // await createCampaign({ ...form, target: ethers.utils.parseUnits(form.target, 18)})
-        //         setIsLoading(false)
-        //         navigate("/")
-        //     } else {
-        //         alert("Provide valid image URL")
-        //         setForm({ ...form, image: "" })
-        //     }
-        // })
-    }
+    // const handleSubmit = async (e) => {
+    //     e.preventDefault()
+    //     // checkIfImage(form.image, async (exists) => {
+    //     //     if (exists) {
+    //     //         setIsLoading(true)
+    //     //         // await createCampaign({ ...form, target: ethers.utils.parseUnits(form.target, 18)})
+    //     //         setIsLoading(false)
+    //     //         navigate("/")
+    //     //     } else {
+    //     //         alert("Provide valid image URL")
+    //     //         setForm({ ...form, image: "" })
+    //     //     }
+    //     // })
+    // }
 
     return (
         <div
@@ -220,39 +227,13 @@ function CreateCampaign() {
                 </div>
 
                 <div class="mt-1 flex justify-center rounded-md border-2 border-dashed border-white-300 px-6 pt-5 pb-6">
-                    <div class="space-y-1 text-center">
-                        <svg
-                            class="mx-auto h-12 w-12 text-white-400"
-                            stroke="white"
-                            fill="none"
-                            viewBox="0 0 48 48"
-                            aria-hidden="true"
-                        >
-                            {/* <path
-                                d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                                stroke-width="2"
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                            /> */}
-                        </svg>
-                        <div class="flex text-sm text-white-600">
-                            <label
-                                for="file-upload"
-                                class="relative cursor-pointer rounded-md py-1 px-2 bg-white font-medium text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 hover:text-indigo-500"
-                            >
-                                <span>Upload a file</span>
-                                <input
-                                    id="file-upload"
-                                    name="file-upload"
-                                    type="file"
-                                    class="sr-only"
-                                />
-                            </label>
-                            <p class="pl-1">or drag and drop</p>
-                        </div>
-                        <p class="text-xs text-white-500">
-                            PNG, JPG, GIF up to 10MB
-                        </p>
+                    <div>
+                        <input type="file" onChange={handleImageChange} />
+                        {image && (
+                            <div>
+                                <img src={image} alt="uploaded image" />
+                            </div>
+                        )}
                     </div>
                 </div>
                 <div>
