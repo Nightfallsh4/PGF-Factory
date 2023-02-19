4c7223db616105d2977b262b66b39c12cebc35de
Shanmugadevan <shanmugadevan@protonmail.com>
2023-02-19 19:05:05 +0530

    Added funding minting

--- src/components/CustomButton.jsx
+++ src/components/CustomButton.jsx
@@ -1,15 +1,108 @@
-import React from 'react'
+import React from "react"
+import { useContractWrite, usePrepareContractWrite, useSigner } from "wagmi"
+import pgfAbi from "../../deployments/pgfFactory.json"
+import { ethers } from "ethers"
+import { makeRoot } from "@/pages/utils/merkleTree"
+// import { CustomFundingButtonInterface } from "../pages/utils/interfaces";
+import Modal from "@mui/material/Modal"
+import { Box, Typography } from "@mui/material"
 
-const CustomButton = ({ btnType, title, handleClick, styles }) => {
-  return (
-    <button
-      type={btnType}
-      className={`font-epilogue font-semibold text-[16px] leading-[26px] text-white min-h-[52px] px-4 rounded-[10px] ${styles}`}
-      onClick={handleClick}
-    >
-      {title}
-    </button>
-  )
+const CustomButton = (params) => {
+    const [open, setOpen] = React.useState(false)
+    const handleOpen = () => setOpen(true)
+    const handleClose = () => setOpen(false)
+    const [hash, setHash] = React.useState("")
+
+    const style = {
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        width: 400,
+        bgcolor: "background.paper",
+        border: "2px solid #000",
+        boxShadow: 24,
+        p: 4,
+    }
+
+    const creationFee = ethers.utils.parseEther("0.0001")
+    // const totalFunding = ethers.utils.parseEther("1")
+    // const withdrawalFee = 1000
+    const isGroupWithdrawal = true
+    const tokenUri = "https://github.com/nightfallsh4"
+    const startTimestamp = Date.now() + 1000
+    const vestingPeriodInSeconds = 60 * 60 * 24 * 30 * parseInt(params.months)
+    const addresses = []
+    params.whitelisted.forEach((element) => {
+        addresses.push(element.value)
+    })
+    const { tree, root } = makeRoot(addresses || ["sc", "dsjf"])
+    const { data: signData, signer } = useSigner()
+    // const { config } = usePrepareContractWrite({
+    //     address: "0xA062E89e79668b873004Bf60c588C899289D4166",
+    //     abi: pgfAbi.abi,
+    //     functionName: "createFunding",
+    //     args: [
+    //         params.totalFunding,
+    //         params.form.withdrawalFee,
+    //         params.isGroupWithdrawal,
+    //         params.tokenUri,
+    //         params.startTimestamp,
+    //         params.form.vestingPeriodInSeconds,
+    //         root,
+    //     ],
+    //     signer: signer,
+    //     overrides: {
+    //         value: ethers.utils.parseEther("0.0001"),
+    //     },
+    // })
+    const { data, isLoading, isSuccess, write, error, isError } =
+        useContractWrite({
+            address: "0xA062E89e79668b873004Bf60c588C899289D4166",
+            abi: pgfAbi.abi,
+            functionName: "createFunding",
+            args: [
+                ethers.utils.parseEther(params.totalFunding || "0"),
+                parseInt(params.withdrawalFee),
+                isGroupWithdrawal,
+                params.tokenUri,
+                startTimestamp,
+                vestingPeriodInSeconds,
+                root,
+            ],
+            overrides: {
+                value: ethers.utils.parseEther("0.0001"),
+            },
+            chainId: 80001,
+            onSuccess(data) {
+                console.log(data.hash)
+                setHash(data.hash)
+            },
+        })
+    async function deploy() {
+        console.log("Clicked deploy")
+        console.log(write)
+        console.log(startTimestamp)
+        console.log(params.withdrawalFee)
+        // console.log(config)
+        console.log(data)
+        console.log(error)
+        console.log(isError)
+        handleOpen()
+        await write?.()
+    }
+    return (
+        <div>
+            <button
+                // type={btnType}
+                className={`font-epilogue font-semibold text-[16px] leading-[26px] text-white min-h-[52px] px-4 rounded-[10px] ${params.styles}`}
+                onClick={deploy}
+            >
+                {params.title}
+            </button>
+            
+        </div>
+    )
 }
 
 export default CustomButton
--- src/pages/utils/interfaces.tsx
+++ src/pages/utils/interfaces.tsx
@@ -1,6 +1,22 @@
-import MerkleTree from "merkletreejs";
+import MerkleTree from "merkletreejs"
 
 export interface MerkleTreeReturn {
-    tree: MerkleTree,
+    tree: MerkleTree
     root: string
+}
+
+export interface CreateFundingInterface {
+    totalFunding: number
+    withdrawalFee: number
+    isGroupWithdrawal: boolean
+    tokenUri: string
+    startTimestamp: number
+    vestingPeriodInSeconds: number
+    merkleRoot: string
+}
+
+export interface CustomFundingButtonInterface extends CreateFundingInterface {
+    title:string,
+    styles:string,
+    whitlelisted:string[],
 }
